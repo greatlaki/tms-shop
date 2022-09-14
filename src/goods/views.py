bfc1ee62bb225d09a8e704778420facef8924a3a
greatlaki <greatlaki911@gmail.com>
2022-09-14 09:14:19 +0300

    products listview

--- src/goods/views.py
+++ src/goods/views.py
@@ -1,8 +1,13 @@
 from rest_framework.generics import ListAPIView
 
-from models import Category
+from models import Category, Product
 
 
 class CategoryListView(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class ProductsListView(ListAPIView):
+    queryset = Product.objects.filter(available=True)
+    serializer_class = ProductListSerializer
