2266a2af74f6f93756bfcf125a7ca25964bb8add
greatlaki <greatlaki911@gmail.com>
2022-09-14 09:22:08 +0300

    feedback generic apiview

--- src/goods/views.py
+++ src/goods/views.py
@@ -1,6 +1,9 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import mixins, status
+from rest_framework.generics import ListAPIView, GenericAPIView
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser, MultiPartParser
 
-from models import Category, Product
+from models import Category, Product, Feedback
 
 
 class CategoryListView(ListAPIView):
@@ -11,3 +14,17 @@ class CategoryListView(ListAPIView):
 class ProductsListView(ListAPIView):
     queryset = Product.objects.filter(available=True)
     serializer_class = ProductListSerializer
+
+
+class FeedbackView(mixins.CreateModelMixin, GenericAPIView):
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerializer
+    parser_classes = (JSONParser, MultiPartParser, )
+
+    def post(self, request, *args, **kwargs):
+        serializer = FeedbackSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'received data': serializer.data}, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
