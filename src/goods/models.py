a077ccab473582dfc41566b5250483576e7f0b3c
greatlaki <greatlaki911@gmail.com>
2022-09-14 09:05:20 +0300

    the model products

--- src/goods/models.py
+++ src/goods/models.py
@@ -14,3 +14,30 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products',on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.URLField(blank=True)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('good:product_detail', kwargs={'slug': self.slug})
+
+    @property
+    def image_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
