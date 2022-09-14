4a6d90e12a7798bbc939baf98d7cd15bbe12aeaf
greatlaki <greatlaki911@gmail.com>
2022-09-14 09:03:18 +0300

    the model category

--- src/goods/models.py
+++ src/goods/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.URLField(blank=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
