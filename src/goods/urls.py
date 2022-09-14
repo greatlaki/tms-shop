b4788ee42a3dbec2be427bd647e6e23142068344
greatlaki <greatlaki911@gmail.com>
2022-09-14 09:34:46 +0300

    new urls

---
+++ src/goods/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from .views import *
+
+urlpatterns = [
+    path('api/feedback/', FeedbackView.as_view(), name='feedback'),
+    path('api/products_list/<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
+    path('api/category/', CategoryListView.as_view(), name='home_page'),
+    path('api/products_list/', ProductsListView.as_view(), name='products_list'),
+
+    path('api/<slug:slug>/', CategoryDetailView.as_view(), name='category_product'),
+]
