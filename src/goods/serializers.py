9b98b5dfd8001dc01306f3f98158debc11ff5c8e
greatlaki <greatlaki911@gmail.com>
2022-09-14 09:24:35 +0300

    category serializer

--- src/goods/serializers.py
+++ src/goods/serializers.py
@@ -1,6 +1,7 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
-from .models import Product
+from .models import Product, Category
 
 
 class ProductListSerializer(ModelSerializer):
@@ -8,3 +9,12 @@ class ProductListSerializer(ModelSerializer):
     class Meta:
         model = Product
         fields = ('slug', 'name', 'image', 'price')
+
+
+class CategorySerializer(ModelSerializer):
+    name = serializers.CharField(required=True)
+    slug = serializers.CharField(required=True)
+
+    class Meta:
+        model = Category
+        fields = '__all__'
