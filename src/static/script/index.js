be50c597a3d6996e7bb825d829ef5f1c78e47666
greatlaki <greatlaki911@gmail.com>
2022-09-14 09:43:15 +0300

    static/script were made

---
+++ src/static/script/index.js
@@ -0,0 +1,17 @@
+function render() {
+    const productsStore = localStorageUtil.getProducts();
+
+    productsPage.render();
+}
+
+let catalog = [];
+
+fetch('setting.json', { mode: 'no-cors'})
+    .then(res => res.json())
+    .then(body => {
+        catalog = body;
+        render();
+    })
+    .catch(error => {
+        console.log(error);
+    });
